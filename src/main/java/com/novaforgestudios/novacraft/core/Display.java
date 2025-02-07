516aed466cd038b2cdca0aec9997666882a8cc3e
Developer-lfierro743 <lfierro743@cbcsd.org>
2025-02-07 05:33:07 +0000

    replaced new stuff

--- src/main/java/com/novaforgestudios/novacraft/core/Display.java
+++ src/main/java/com/novaforgestudios/novacraft/core/Display.java
@@ -1,22 +1,36 @@
 package com.novaforgestudios.novacraft.core;
 
 import java.awt.Canvas;
+import java.awt.Dimension;
 import javax.swing.JFrame;
 
 public class Display extends Canvas {
 
-    int WIDTH = 800;
-    int HEIGHT = 600;
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
+    public static final String TITLE = "Novacraft Pre-Development Version";
+
+    public Display() {
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    }
 
     public static void main(String[] args) {
         Display game = new Display();
-        JFrame frame = new JFrame();
+        JFrame frame = new JFrame(TITLE);
+
+        // Configure the JFrame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
-        frame.setSize(game.WIDTH, game.HEIGHT);
         frame.add(game);
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+
+        
+        game.start();
+    }
+
+    public void start() {
+        System.out.println("Game started!");
     }
 }
