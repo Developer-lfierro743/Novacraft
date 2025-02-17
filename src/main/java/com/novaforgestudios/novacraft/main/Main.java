daafef15893d24d00d1afeb205e8bc54132c7bce
Developer-lfierro743 <lfierro743@cbcsd.org>
2025-02-17 00:07:15 +0000

    updated new code and alot more

--- src/main/java/com/novaforgestudios/novacraft/main/Main.java
+++ src/main/java/com/novaforgestudios/novacraft/main/Main.java
@@ -1,31 +1,40 @@
 package com.novaforgestudios.novacraft.main;
 
-public class Main implements Runnable {
+import engine.io.Window;
+
+ public class Main implements Runnable {
  public Thread game;
-	
- public void start() {
-  game = new Thread(this, "Novacraft");
-  game.start();
- }
-	
- public static void init() {
-  System.out.println("Initializing Game!");
+ public Window window;
+ public final int WIDTH = 1280, HEIGHT = 760;
+    
+  public void start() {
+   game = new Thread(this, "game");
+   game.start();
+  }
+    
+  public void init() {
+   System.out.println("Initializing Game!");
+   window = new Window(WIDTH, HEIGHT,"NOVACRAFT INDUSTRIAL REVOLUTION Edition");
+   window.create();
+
+  
  }
 	
  public void run() {
   init();
-  while (true) {
+  while (!window.shouldClose()) {
    update();
    render();
   } 
  }
 	
  private void update() {
-  System.out.println("updating game!");
- }
-	
+  // System.out.println("Updating Game!");
+  window.update();
+  }
  private void render() {
-  System.out.println("Rendering Game!");
+  // System.out.println("Rendering Game!");
+  window.swapBuffers();
  }
 	
  public static void main(String[] args) {
