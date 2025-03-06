fccbcd049d752db6acf5726fb2aab63f22940d91
Developer-lfierro743 <lfierro743@cbcsd.org>
2025-03-06 00:55:51 +0000

    rewriting whole code structure

---
+++ Novacraft/src/test/java/com/novacraft/AppTest.java
@@ -0,0 +1,11 @@
+package com.novacraft;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    void testAppInitialization() {
+        assertTrue(true); // Placeholder test
+    }
+}
